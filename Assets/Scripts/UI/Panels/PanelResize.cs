718237cc2aeb4990bcf95cf24270bc20781f4adb
Michael Pollind <mpollind@gmail.com>
2016-05-23 22:04:38 -0700

    re-organized panels

---
+++ Assets/Scripts/UI/Panels/PanelResize.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PanelResize : MonoBehaviour , IEventSystemHandler,IDragHandler,IPointerDownHandler,IPointerUpHandler
+{
+	public RectTransform rectTransform{ get; private set; }
+	public PanelFrame panelFrame{ get; private set; }
+
+	private Vector2 localPosition = Vector2.zero;
+
+	void Awake()
+	{
+		rectTransform = this.GetComponent<RectTransform> ();
+		panelFrame = this.GetComponentInParent<PanelFrame> ();
+	}
+
+
+	public void OnDrag (PointerEventData eventData)
+	{
+	}
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+	}
+
+	public void OnPointerUp (PointerEventData eventData)
+	{
+	}
+}
+
